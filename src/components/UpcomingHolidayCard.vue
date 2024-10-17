99a0ede6548eb832b7dfb692994f49006d17ca21
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-10-17 02:51:40 +0200

    Widget section was done ooooo

--- src/components/UpcomingHolidayCard.vue
+++ src/components/UpcomingHolidayCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// Define props with type for item
 const props = defineProps<{
   item: {
     country: {
@@ -14,11 +15,17 @@ const props = defineProps<{
 
 <template>
   <div class="py-4 px-3 border border-black rounded">
+    <!-- Display country name -->
     <h2 class="text-xl">{{ props.item.country.name }}</h2>
-    <p v-if="item.nextHoliday">
-      <span class="font-bold">Next Holiday:</span> {{ item.nextHoliday.name }}
+
+    <!-- Check if nextHoliday exists and display its details -->
+    <p v-if="props.item.nextHoliday">
+      <span class="font-bold">Next Holiday:</span>
+      {{ props.item.nextHoliday.name }}
     </p>
     <p v-else>No upcoming holidays</p>
-    <p>{{ item.nextHoliday?.date }}</p>
+
+    <!-- Display holiday date if nextHoliday exists -->
+    <p v-if="props.item.nextHoliday">{{ props.item.nextHoliday.date }}</p>
   </div>
 </template>
