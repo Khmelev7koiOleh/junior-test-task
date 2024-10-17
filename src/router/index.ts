21e7fef54a5785f7c8210d4d7cdc24fc963ddb38
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-10-17 03:04:31 +0200

    Widget section was done oooooooo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Create and configure the router
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      component: () => import('../views/HomeView.vue'), // Home view component
     },
     {
-      path: '/countries/:name-:countryCode',
+      path: '/countries/:name-:countryCode', // Dynamic path for country routes
       name: 'country',
-      component: () => import('../views/countries/[name].vue'),
+      component: () => import('../views/CountryView.vue'), // Direct reference to the view file
     },
   ],
 })
