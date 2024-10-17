283217b7723516701df958f88b8325c5180689bd
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-10-17 03:00:00 +0200

    Widget section was done ooooooo

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
-
 import { RouterLink } from 'vue-router'
 
+// Props definition
 const props = defineProps<{
-  country: Country
+  country: {
+    name: string
+    countryCode: string
+  }
   index: number
 }>()
 </script>
 
 <template>
-  <router-link
+  <RouterLink
     :to="`/countries/${encodeURIComponent(country.name)}-${country.countryCode}`"
   >
-    <div class="w-[35vw] h-30 border py-5 border-black">
-      <p class="text-center">
-        {{ props.country.name }} - {{ props.index + 1 }}
-      </p>
+    <div
+      class="w-[35vw] h-30 border py-5 border-black hover:bg-gray-100 transition"
+    >
+      <p class="text-center">{{ country.name }} - {{ index + 1 }}</p>
     </div>
-  </router-link>
+  </RouterLink>
 </template>
